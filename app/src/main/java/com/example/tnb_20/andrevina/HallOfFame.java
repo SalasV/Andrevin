a2f47299154b2931df24f01467164aea37b49f46
Victor Salas Fernandez <suep2525@gmail.com>
2018-11-19 20:16:08 +0100

    Persistencia Ficheros

--- app/src/main/java/com/example/tnb_20/andrevina/HallOfFame.java
+++ app/src/main/java/com/example/tnb_20/andrevina/HallOfFame.java
@@ -1,17 +1,25 @@
 package com.example.tnb_20.andrevina;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 
-public class HallOfFame extends Activity{
+public class HallOfFame extends Activity {
+   private List<Jugador> jugadors;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +27,40 @@ public class HallOfFame extends Activity{
         mostrarDatos();
     }
 
-    private void mostrarDatos(){
-        List<Jugador> jugadors = MainActivity.jugador;
+    private void mostrarDatos() {
+        jugadors = new ArrayList<>();
+        leerFicheros();
 
         final TextView tablaRecord = findViewById(R.id.record);
         tablaRecord.setText("");
-        if(jugadors.size()>0){
+        if (jugadors.size() > 0) {
             Collections.sort(jugadors);
-            for (Jugador jug:
+            for (Jugador jug :
                     jugadors) {
                 tablaRecord.setText(tablaRecord.getText() + jug.toString());
             }
-        }else{
+        } else {
             tablaRecord.setText(tablaRecord.getText() + "No hay datos registrados");
         }
 
     }
+
+    private void leerFicheros() {
+        try {
+            BufferedReader br=
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    openFileInput("tablaRecord.txt")));
+            String linea;
+            while ((linea=br.readLine())!=null){
+                String[] cadena=linea.split("-");
+                jugadors.add(new Jugador(cadena[0],Integer.parseInt(cadena[1])));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/com/example/tnb_20/andrevina/MainActivity.java
+++ app/src/main/java/com/example/tnb_20/andrevina/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private int rango;
     public static List<Jugador> jugador = new ArrayList<>();
-    private int intentos  = 0;
+    private int intentos = 0;
     private String name;
 
     @Override
@@ -75,32 +78,32 @@ public class MainActivity extends AppCompatActivity {
             toast.show();
             intentos++;
         } else if (numero == rango) {
-            jugador.add(new Jugador(name,intentos));
+            jugador.add(new Jugador(name, intentos));
+            fichero(new Jugador(name, intentos));
             Context context = getApplicationContext();
             CharSequence text = "Lo has adivinado";
             int duration = Toast.LENGTH_SHORT;
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
             rango = generateRandom();
-            intentos=0;
+            intentos = 0;
             preguntarNombre();
         }
     }
 
     public void tablaRecord() {
         Intent i = new Intent(this, HallOfFame.class);
-
         startActivity(i);
     }
 
-    private String preguntarNombre(){
+    private String preguntarNombre() {
         final Dialog dialog = new Dialog(MainActivity.this);
 
         dialog.setContentView(R.layout.dialog);
         dialog.setTitle("Registro de Usuario");
         dialog.show();
         Button register = dialog.findViewById(R.id.botonDialog);
-        register.setOnClickListener(new View.OnClickListener(){
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText textName = dialog.findViewById(R.id.etNombre);
@@ -108,7 +111,22 @@ public class MainActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
-
         return name;
+    }
+
+    private void fichero(Jugador jug) {
+        try {
+            OutputStreamWriter osw =
+                    new OutputStreamWriter(
+                            openFileOutput("tablaRecord.txt", Context.MODE_APPEND));
+
+            osw.write(jug.getName() + "-" + jug.getIntentos());
+            osw.append("\r\n");
+            osw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
